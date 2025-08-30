b41c63339812da5eab59bf6a28dd42ab527b6d23
Your NameMueez <mueezimran1@gmail.com>
2025-08-30 23:13:14 +0500

    thanks

--- src/components/OrderThankYou.jsx
+++ src/components/OrderThankYou.jsx
@@ -10,18 +10,18 @@ const OrderThankYou = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-pink-50 p-6 text-center">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[#a4c2da] p-6 text-center">
       <div className="bg-white p-8 rounded-2xl shadow-xl max-w-md w-full border-4 border-dashed border-pink-200">
-        <div className="text-pink-500 text-5xl mb-4">
+        <div className="text-[#1c5078] text-5xl mb-4">
           <FaHeart className="mx-auto animate-bounce" />
         </div>
-        <h1 className="text-2xl font-bold text-gray-800 mb-2">Thankyou For Ordering Luvvv 🍓</h1>
+        <h1 className="text-2xl font-bold text-gray-800 mb-2">Thankyou For Ordering Luvvv </h1>
         <p className="text-gray-600 mb-6">
           Your order has been placed successfully. 
         </p>
         <button
           onClick={handleContinueShopping}
-          className="bg-pink-400 hover:bg-pink-500 text-white font-semibold px-6 py-3 rounded-full transition duration-300 shadow-md"
+          className="bg-[#1c5078] hover:bg-pink-500 text-white font-semibold px-6 py-3 rounded-full transition duration-300 shadow-md"
         >
           Continue Shopping
         </button>
